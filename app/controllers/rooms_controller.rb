ba25408236e9ac18a084500ad112250931b5e69f
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-12-12 14:37:55 +0900

    rooomコントローラー定義

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,43 @@
 class RoomsController < ApplicationController
   def show
+    @room = Room.find(params[:id])
+    @message = Message.new
+    @messages = @room.messages
+    if @room.user.id == current_user.id
+      @shop = @room.shop
+    else
+      redirect_to "root_path"
+    end
+  elsif shop_signed_in?
+    if @room.shop.id == current_user.id
+      @user = @room.user
+    else
+      redirect_to "root_path"
+    end
+
+  else
+    redirect_to "root_path"
+  end
+end
+
+  def create
+    if user_signed_in?
+      @room = Room.new(room_shop_params)
+      @room.user_id = current_user.id
+    elsif shop_signed_in?
+      @room = Room.new(room_user_params)
+      @room.shop_id = current_shop.id
+    else
+      redirect_to "root_path"
+    end
+  end
+
+  private
+  def room_shop_params
+    params.require(:room).permit(:shop_id)
+  end
+
+  def room_user_params
+    params.require(:room).permit(:user_id)
   end
 end
