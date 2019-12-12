3c7be097d1981ca11583156975865aa63f38632a
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-12-12 15:15:53 +0900

    kannsei

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,25 +1,23 @@
 class MessagesController < ApplicationController
 
   def create
-    @room Room.find(params[:room_id])
+    @room = Room.find(params[:room_id])
     @message = Message.new(message_params)
+    #メッセージがuserによるものだったらis_user=true, shopによるものだったらis_user=false
+    if user_signed_in?
+      @message.is_user = true
+    elsif shop_signed_in?
+      @message.is_user = false
+    end
+    @message.room_id = @room.id
+    if @message.save
+      redirect_to room_url(@room)
+    else
+      redirect_to room_url(@room)
+    end
   end
 
-  if user_signed_in?
-    @message.is_user = true
-  elsif shop_signe_in?
-    @message.is_user = false
-  end
-
-  @mesasge.room_id = @room.room.id
-  if @message.save
-    redirect_to room_url(@room)
-  else
-    redirect_to room_url(@room)
-  end
-end
-
-private
+  private
   def message_params
     params.require(:message).permit(:content)
   end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,42 +1,44 @@
 class RoomsController < ApplicationController
   def show
-    @room = Room.find(params[:id])
-    @message = Message.new
-    @messages = @room.messages
-    if @room.user.id == current_user.id
-      @shop = @room.shop
-    else
-      redirect_to "root_path"
-    end
-  elsif shop_signed_in?
-    if @room.shop.id == current_user.id
-      @user = @room.user
-    else
-      redirect_to "root_path"
-    end
+    @room = Room.find(params[:id]) #ルーム情報の取得
+    @message = Message.new #新規メッセージ投稿
+    @messages = @room.messages #このルームのメッセージを全て取得
+    if user_signed_in?
+      if @room.user.id == current_user.id
+        @shop = @room.shop
+      else
+        redirect_to "/"
+      end
+    elsif shop_signed_in?
+      if @room.shop.id == current_shop.id
+        @user = @room.user
+      else
+        redirect_to "/"
+      end
 
-  else
-    redirect_to "root_path"
+    else
+      redirect_to "/"
+    end
   end
-end
 
   def create
     if user_signed_in?
+      #userがログインしてたらuser_idを, shopがログインしてたらshop_idを@roomにいれる
       @room = Room.new(room_shop_params)
       @room.user_id = current_user.id
     elsif shop_signed_in?
       @room = Room.new(room_user_params)
       @room.shop_id = current_shop.id
     else
-      redirect_to "root_path"
+      redirect_to "/"
     end
 
-  if @room.save
-    redirect_to action: "show", id: @room.id
-  else
-    redirect_to "root_path"
+    if @room.save
+      redirect_to :action => "show", :id => @room.id
+    else
+      redirect_to "/"
+    end
   end
-end
 
   private
   def room_shop_params
