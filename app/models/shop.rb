87e77e77b026d6c5c2bd490e1b6315aad19ffb97
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-12-12 14:14:55 +0900

    アソシエーション設定

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,4 +3,6 @@ class Shop < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+        has_many :rooms
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_044256) do
+ActiveRecord::Schema.define(version: 2019_12_12_050602) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "room_id"
+    t.boolean "is_user"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "shop_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_rooms_on_shop_id"
+    t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
 
   create_table "shops", force: :cascade do |t|
     t.string "email", default: "", null: false
