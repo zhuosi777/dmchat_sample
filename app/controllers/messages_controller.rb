ef04c9d6c613c802547ad52f9c6bcb981376455a
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-12-12 14:56:17 +0900

    messageコントローラー定義

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,26 @@
 class MessagesController < ApplicationController
+
+  def create
+    @room Room.find(params[:room_id])
+    @message = Message.new(message_params)
+  end
+
+  if user_signed_in?
+    @message.is_user = true
+  elsif shop_signe_in?
+    @message.is_user = false
+  end
+
+  @mesasge.room_id = @room.room.id
+  if @message.save
+    redirect_to room_url(@room)
+  else
+    redirect_to room_url(@room)
+  end
+end
+
+private
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
